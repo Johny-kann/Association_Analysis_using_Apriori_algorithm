8a61814641d8c85eb4fc857bc0b8e8102272be54
Johny-kann <johny.manasee@gmail.com>
2015-03-12 01:44:03 -0400

    Deleted unnecessary
    Deleted many unnecessary files

--- src/com/data_mining/controller/MainController.java
+++ src/com/data_mining/controller/MainController.java
@@ -11,14 +11,10 @@ import com.data_mining.logic.AssociationLogics;
 import com.data_mining.logic.AttributeAndRecordLoaders;
 import com.data_mining.logic.ChoosingAttributes;
 import com.data_mining.logic.CommonLogics;
-import com.data_mining.logic.SearchingLogics;
 import com.data_mining.logs.TrainingLog;
 import com.data_mining.model.association.AssociationList;
 import com.data_mining.model.association.FrequentAndCandidateItemSet;
 import com.data_mining.model.association.TransactionTable;
-import com.data_mining.model.attributes_records.DataTable;
-import com.data_mining.model.attributes_records.OrderedClassSet;
-import com.data_mining.model.rules.RuleSet;
 import com.data_mining.view.console.Outputs;
 
 
@@ -30,19 +26,10 @@ import com.data_mining.view.console.Outputs;
  */
 public class MainController {
 
-	DataTable mainAttributes;
-	DataTable trainData;
-	DataTable validationData;
-	DataTable testData;
-	RuleSet mainRuleSet;
-	OrderedClassSet sortedClassSet;
 	TransactionTable transTable;
 	
 	public MainController()
 	{
-		mainAttributes = new DataTable();
-		mainRuleSet = new RuleSet();
-		testData = new DataTable();
 		PropertiesConfig.assignInputFiles();
 	
 	}
@@ -52,6 +39,7 @@ public class MainController {
 	public void loadTable()
 	{
 		TransactionTable temp = new TransactionTable();
+		
 		AttributeAndRecordLoaders.loadAttributeFromFile(temp, FilesList.ATTRIBUTES_FILES, FilesList.RECORD_FILES);
 		transTable = temp;
 	}
@@ -82,132 +70,6 @@ public class MainController {
 	}
 	
 	
-	public void fillRuleSet()
-	{
-		CommonLogics cl = new CommonLogics();
-
-		
-	//	System.out.println(classes.getOrderedClasses());
-	ChoosingAttributes choose = new ChoosingAttributes();
-	if(Notations.PRUNING_ON)
-	{
-		sortedClassSet = new OrderedClassSet(
-				cl.sortMapValues
-				(cl.classAndCounts(trainData))
-				);
-		TrainingLog.trainLogs.info("Sorted the input classes");
-		
-		Outputs.printToConsole("Order of the classes "+ sortedClassSet.getClassesAlone()
-				);
-		
-	mainRuleSet = choose.fillRuleSet(trainData, sortedClassSet,validationData);
-	orderBasedOnGeneralError();
-	}else
-	{
-	//	System.out.println(mainAttributes.sizeOfRecords()+","+trainData.sizeOfRecords()+","+testData.sizeOfRecords());
-		Map<String,Integer> map = cl.classAndCounts(mainAttributes);
-		
-	
-		sortedClassSet = new OrderedClassSet(
-				cl.sortMapValues
-				(map)
-				);
-		
-		Outputs.printToConsole("Order of the classes "+ sortedClassSet.getClassesAlone()
-				);
-		mainRuleSet = choose.fillRuleSet(mainAttributes, sortedClassSet,validationData);
-	}
-	
-	}
-	
-	public void getResultSet()
-	{
-		
-	}
-	
-	public void orderBasedOnGeneralError()
-	{
-		new CommonLogics().sortRulesBasedonGeneralizationError(mainRuleSet);
-		
-	}
-	
-	public void output()
-	{
-		trainDataAccuracy();
-		
-		if(Notations.TEST_ON)
-		{
-			testDataAccuracy();
-		}
-		mainRuleSetPrint();
-	}
-	
-	public void trainDataAccuracy()
-	{
-		StringBuffer stBuffer = new StringBuffer();
-
-		stBuffer.append(new Outputs().outputRuleSet(mainRuleSet));
-		stBuffer.append("Train Data");
-		stBuffer.append(System.lineSeparator());
-		
-				stBuffer.append(new Outputs().outPutTable(mainAttributes));
-	
-		stBuffer.append("Accuracy "+
-		new ChoosingAttributes().AccuracyForTableByRuleSet(mainAttributes, mainRuleSet,stBuffer)
-				);
-
-		Outputs.printToConsole(stBuffer.toString());
-		new TextFileWriter().writeFile(stBuffer.toString(), FilesList.WRITE_TRAIN_RESULT);
-			
-	}
-	
-	public void mainRuleSetPrint()
-	{
-		StringBuffer stBuffer = new StringBuffer();
-		new TextFileWriter().writeFile(
-				new Outputs().outputRuleSet(mainRuleSet), 
-				FilesList.WRITE_RULE_SET) ;
-	}
-	
-	public void testDataAccuracy()
-	{
-		AttributeAndRecordLoaders.loadAttributeFromFile(testData, FilesList.ATTRIBUTES_FILES, FilesList.TEST_RECORD_FILES);
-		TrainingLog.testLogs.info("Accuracy Loaded from file");
-		StringBuffer stBuffer = new StringBuffer();
-		
-			
-		Outputs.printToConsole(
-		new Outputs().outPutTable(testData)
-		);
-		stBuffer.append("Test Data");
-		stBuffer.append(System.lineSeparator());
-		stBuffer.append(new Outputs().outPutTable(testData)
-				);
-		stBuffer.append("Accuracy "+
-				new ChoosingAttributes().AccuracyForTableByRuleSet(testData, mainRuleSet,stBuffer));
-		
-		Outputs.printToConsole(stBuffer.toString());
-		new TextFileWriter().writeFile(stBuffer.toString(), FilesList.WRITE_TEST_RESULT);
-	}
-	
-	
-	public DataTable getMainTable()
-	{
-		return mainAttributes;
-	}
-	
-	public DataTable getTrainAttributes()
-	{
-		return trainData;
-	}
-	
-	public DataTable getTestAttributes() {
-		return validationData;
-	}
-
-	public OrderedClassSet getSortedClassSet() {
-		return sortedClassSet;
-	}
 	
 	
 }
--- src/com/data_mining/logic/CommonLogics.java
+++ src/com/data_mining/logic/CommonLogics.java
@@ -18,12 +18,6 @@ import com.data_mining.constants.Notations;
 import com.data_mining.constants.ValueConstants;
 import com.data_mining.logs.TrainingLog;
 import com.data_mining.model.association.FrequentAndCandidateItemSet;
-import com.data_mining.model.attributes_records.DataTable;
-import com.data_mining.model.attributes_records.Records;
-import com.data_mining.model.errors.PassingAttribute;
-import com.data_mining.model.rules.RuleCondition;
-import com.data_mining.model.rules.RuleSet;
-import com.data_mining.model.rules.Rules;
 
 /**
  * @author Janakiraman
@@ -32,27 +26,6 @@ import com.data_mining.model.rules.Rules;
 public class CommonLogics {
 
 	
-	
-	/**
-	 * 
-	 * Removes a record from the list and gives a new list unaffected by the old list
-	 * @param original
-	 * @return
-	 */
-	public List<String> removeElement(List<String> original,int index)
-	{
-		List<String> newList = new ArrayList<String>();
-		
-		for(int i=0;i<original.size();i++)
-		{
-			if(i!=index)
-			{
-				newList.add(original.get(i));
-			}
-		}
-		
-		return newList;
-	}
 	
 	public static void assignInitValues(String args[])
 	{
@@ -178,58 +151,11 @@ public class CommonLogics {
 		return set;
 	}
 	
-	/**
-	 * 
-	 * Removes a record from the list and gives a new list unaffected by the old list
-	 * @param original
-	 * @return refined record
-	 */
-	public Records removeElementFromRecordDiscrete(Records original,int index)
-	{
-		Records newRecords = new Records(
-				removeElement(original.getElements(), index),
-				original.getClassAttribute());
-		
-				
-		return newRecords;
-	}
 	
 	
 
 	
-	/**
-	 * @param table
-	 * @param class value
-	 * @return count of a particular class value
-	 */
-	public Integer getCountOfClassValue(DataTable table,String value)
-	{
-		Integer sum=0;
-		for(int i=0;i<table.sizeOfRecords();i++)
-		{
-			if(table.getRecordAtIndex(i).getClassAttribute().equals(value))
-				sum++;
-		}
-		
-		return sum;
-	}
 	
-	/**
-	 * @param table
-	 * @param class value
-	 * @return count of a particular class value
-	 */
-	public Integer getCountOfOtherClassValues(DataTable table,String value)
-	{
-		Integer sum=0;
-		for(int i=0;i<table.sizeOfRecords();i++)
-		{
-			if(!(table.getRecordAtIndex(i).getClassAttribute().equals(value)))
-				sum++;
-		}
-		
-		return sum;
-	}
 	
 	
 	public Integer getValueofMapAtIndex(Map<String,Integer> map,int index)
@@ -239,353 +165,20 @@ public class CommonLogics {
 		
 	}
 	
-	/**
-	 * Total classes and their counts
-	 * @param table
-	 * @return map of classes and counts
-	 */
-	public Map<String,Integer> classAndCounts(DataTable table)
-	{
-		Map<String,Integer> categories = new LinkedHashMap<String, Integer>();
-		
-		for(int i=0;i<table.getClassValues().size();i++)
-		{
-			categories.put(
-					table.getClassValues().get(i),
-					getCountOfClassValue(
-							table, table.getClassValues().get(i)
-							));
-		}
-		
-		return categories;
-		
-	}
-	
-	public void removeRecords(DataTable input,DataTable remover)
-	{
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		
-		for(int i=0;i<remover.sizeOfRecords();i++)
-		{
-			for(int j=0;j<input.sizeOfRecords();j++)
-			{
-				if(equalRecords(input.getRecordAtIndex(j),
-						remover.getRecordAtIndex(i)))
-				{
-				array.add(j);
-				}
-			}
-		}
-		
-		Collections.sort(array);
-		Collections.reverse(array);
-		
-		
-		for(int i:array)
-		{
-			input.getRecords().remove(i);
-		}
-		
-		
-	}
-	
-	public boolean equalRecords(Records rec1,Records rec2)
-	{
-		Boolean good=true;
-		
-		for(int i=0;i<rec1.getElements().size();i++)
-		{
-			if(rec1.getElements().get(i)!=rec2.getElements().get(i))
-			{
-				good=false;
-				break;
-			}
-		}
-		
-		if(rec1.getClassAttribute()!=rec2.getClassAttribute())
-		{
-			good=false;
-			
-		}
-		
-		return good;
-	}
 	
 	
 	
-	public void removeRecordByAttrbValue(DataTable table,int index,String value)
-	{
-		for(int i=0;i<table.sizeOfRecords();i++)
-		{
-			if(table.getRecordAtIndex(i).getElementValueAtIndex(index)==value)
-			{
-				table.getRecords().remove(i);
-			}
-		}
-	}
-	
-	/**
-	 * sorts the table according to the attribute index
-	 * @param table
-	 * @param attribute
-	 */
-	public void sort(DataTable temp,int index)
-	{
-		Records tem;
-		
-		List<Records> recs = temp.getRecords();
-	//	InputGetter.consoleOutPut(recs.size());
-		for(int i=0;i<recs.size();i++)
-		{
-			for(int j=i+1;j<recs.size();j++)
-			{
-			//	InputGetter.consoleOutPut(Double.parseDouble(recs.get(j).getElementValueAtIndex(index)));
-				if(Double.parseDouble(recs.get(j).getElementValueAtIndex(index))
-						<Double.parseDouble(recs.get(i).getElementValueAtIndex(index))
-						)
-				{
-					tem = recs.get(j);
-					recs.set(j, recs.get(i));
-					recs.set(i, tem);
-					
-				}
-			}
-		}
-	}
 	
 	
-	public void sortRulesBasedonGeneralizationError(RuleSet ruleSet)
-	{
+	
+	
+	
 	 
-		List<Rules> temp = ruleSet.getRulesList();
-		
-		for(int i=0;i<temp.size();i++)
-		{
-			for(int j=i;j<temp.size();j++)
-			{
-				if(temp.get(i).getgError()>temp.get(j).getgError())
-				{
-					Collections.swap(temp, i, j);
-					
-				}
-			}
-		}
-		
-		
-	}
 	
 	
 	
-	/**
-	 * @param table
-	 * @param attributeIndex
-	 * @return splitted values for continuous attribute
-	 */
-	public List<Double> fillSplitList(DataTable table,int attributeIndex)
-	{
-		List<Records> temp = table.getRecords();
-		List<Double> splits = new ArrayList<Double>();
-		
-		for(int i=0;i<temp.size();i++)
-		{
-			if(i==0)
-			{
-				splits.add(Double.parseDouble(
-						temp.get(i).getElementValueAtIndex(attributeIndex))
-						*0.8);
-				
-			}
-			else
-			{
-				splits.add(
-						 ((
-								 Double.parseDouble(
-								 temp.get(i).getElementValueAtIndex(attributeIndex))+
-								 Double.parseDouble(
-								 temp.get(i-1).getElementValueAtIndex(attributeIndex))
-								 )/2.0)
-						);
-			}
-		}
-		splits.add(Double.parseDouble((temp.get
-				(temp.size()-1).
-				getElementValueAtIndex(attributeIndex)))
-				*1.2
-				);
-		
-		return splits;
-	}
-	 
-	/**
-	 * @param table
-	 * @return list of positions where split can occur
-	 */
-	public List<Integer> splitPostition(DataTable table)
-	{
-		List<Integer> list = new ArrayList<Integer>();
-		
-		for(int i=1;i<table.sizeOfRecords();i++)
-		{
-			if(!(
-			table.getRecordAtIndex(i).getClassAttribute().equals
-			(table.getRecordAtIndex(i-1).getClassAttribute()
-					)))
-			{
-				list.add(i);
-			}
-		}
-		
-		return list;
-	}
 	
-	/**
-	 * finds the best rule from the list of rule condition and returns the best rule condition
-	 * @param List of rule conditions
-	 * @return best rule condition
-	 */
-	public RuleCondition bestAttributeFromErrorModel(List<RuleCondition> rules) throws IndexOutOfBoundsException
-	{
-		RuleCondition index = null;
-		
-		if(Notations.STRICTLY_LAPLACE==false)
-		{
-			TrainingLog.accuracyLogs.info("Inside strictly_laplace false block");
-		for(RuleCondition rule:rules)
-		{
-		
-			if(rule.getNoOfCorrectClass()>0)
-			{
-				index = rule;
-				break;
-			}
-		}
-		try
-		{
-		TrainingLog.accuracyLogs.info("Rule chosen in strictly_laplace false after for loop "+index.getCondition());
-		}catch(NullPointerException ne)
-		{
-			TrainingLog.accuracyLogs.info("Null pointer exception index not assigned any value");
-		}
-		}
-		
-		else
-		{
-			TrainingLog.accuracyLogs.info("Inside strictly_laplace true block");
-	//		Double laplace = rules.get(0).getError();
-			index = rules.get(0);
-			TrainingLog.accuracyLogs.info("Rule chosen in else"+index.getCondition());
-		}
 	
-		for(RuleCondition rule:rules)
-		{
-		
-		
-			if(index.getError()<rule.getError()
-					)
-			{
-				index = rule;
-			}
-		}
-		
-		
-		TrainingLog.accuracyLogs.info("Rule chosen "+index.getCondition());
-		
-		return index;
-		
-		}
-	
-	/**
-	 * @param table
-	 * @return if it belongs to a leaf node
-	 */
-	public boolean isleafNode(DataTable input)
-	{
-		ChoosingAttributes cr = new ChoosingAttributes();
-		Double error = cr.calculateErrorForTable(input);
-//		System.out.println("Error for leaf node " + error);
-		
-		if(error == 0.0)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	/**
-	 * @param map of classes and counts
-	 * @return best class
-	 */
-	public String bestClassFromMap(Map<String,Integer> input)
-	{
-		String index = null;
-	//	Double error=1.0;
-		Integer count = 0;
-		
-		Set<String> keys = input.keySet();
-	
-		for(String key:keys)
-		{
-			if(count<input.get(key))
-				{
-			
-				count=input.get(key);
-				index = key;
-				
-				}
-		}
-					
-		return index;
-		
-	}
-	
-	/**
-	 * @param table
-	 * @param error
-	 * @return finds the maximum occurred class
-	 */
-	public String findMaxClass(DataTable inputRecords,Double error)
-	{
-		String classSelected;
-		CommonLogics cl = new CommonLogics();
-		Map<String,Integer> map = cl.classAndCounts(inputRecords);
-		
-		classSelected = cl.bestClassFromMap(map);
-	
-		return classSelected;
-	}
-	
-	/**
-	 * @param map of attributes and their split errors
-	 * @return best attribute
-	 */
-	public String bestAttributeFromMap(Map<String,Double> input)
-	{
-		String index = null;
-		Double error=1.0;
-		
-		Set<String> keys = input.keySet();
-		
-	
-		
-		for(String key:keys)
-		{
-			if(error>input.get(key))
-				{
-			
-				error=input.get(key);
-				index = key;
-				
-				}
-		}
-		
-			
-		return index;
-		
-	}
-
 	/**
 	 * @param Node condition
 	 * @return Node value
